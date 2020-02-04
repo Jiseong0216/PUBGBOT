061f5b3cbcf5a75ed238046f61a52d2dd8c61811
Jiseong0216 <60600022+Jiseong0216@users.noreply.github.com>
2020-02-04 18:19:23 +0900

    Update 배그봇.py

--- 배그봇.py
+++ 배그봇.py
@@ -55,17 +55,11 @@ async def on_message(message):
         embed = discord.Embed()
         embed.add_field(name="오늘의 공지!", value="공식 서버가 될 날이 얼마 안남았습니다.\n조금만 더 기다려 주세요!")
         await message.channel.send(embed=embed)
- 
+        
 @client.event
-async def sendMessage(color, title, value, should_delete=True, channel=""):
-    embed = discord.Embed(color=color)
-    embed.add_field(name=title, value=value, inline=False)
-    if channel == "":
-        msg = await client.send_message(message_obj.channel, embed=embed)
-    else:
-        msg = await client.send_message(client.get_channel(channel), embed=embed)
-    if should_delete:
-        await delete_msg(msg)
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name='NEW!')
+    await client.add_roels(member, role)
 
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
